8f2db92340bed8d22d53b5199cb30ae88aa4844d
ignacio_almonte_santos <lilsaint080@gmail.com>
2025-03-13 13:24:41 +0100

    Added method by owner

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+
+  public void methodAddedByOwner(){
+    System.out.println("Method Added By Owner");
+  }
   /**
    * Retrieves the day component of this date.
    *
